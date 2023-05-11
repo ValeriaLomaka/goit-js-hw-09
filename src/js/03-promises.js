e60042fc03877ff7dec99b4b79e5811c58485e7e
VALERIA <valerialomaka04@gmail.com>
2023-05-11 20:13:51 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,33 +4,44 @@ const form = document.querySelector('.form');
 
 form.addEventListener('submit', onBtnClick);
 
-function onBtnClick(e) {
-  e.preventDefault();
+const data = {};
 
-  let delay = Number(e.currentTarget.elements.delay.value);
-  let step = Number(e.currentTarget.elements.step.value);
-  const amount = Number(e.currentTarget.elements.amount.value);
-  
-  for (let i = 0; i < amount; i++) {
-    delay += step;
-    createPromise(i, delay)
+function onBtnClick(evt) {
+  evt.preventDefault();
+  makeParams(evt);
+  createPromisesChain();
+}
+
+function makeParams(evt) {
+  const formData = new FormData(evt.currentTarget);
+  formData.forEach((value, key) => {
+    data[key] = +value;
+  });
+}
+
+function createPromisesChain() {
+  for (let i = 1; i <= data.amount; i+=1) {
+    createPromise(i, data.delay)
       .then(({ position, delay }) => {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
+    data.delay += data.step;
   }
 }
-  function createPromise(position, delay) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const shouldResolve = Math.random() > 0.3;
-        if (shouldResolve) {
-          resolve({ position, delay });
-        } else {
-          reject({ position, delay });
-        }
-      }, delay);
-    });
-  }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
